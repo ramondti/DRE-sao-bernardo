c1555dc3c2c92ea6039b4413b46eb8198f337be2
ramondti <ramon@dataintegra.com.br>
2022-08-29 16:43:12 -0300

    Ajuste dia 29/08

--- src/database/db.js
+++ src/database/db.js
@@ -10,8 +10,14 @@ export default knex({
     password: process.env.DB_PASSWORD,
     connectString: process.env.DB_CONNECTION,
     pool: {
-      min: 1,
-      max: 3,
+      min: 2,
+      max: 6,
+      createTimeoutMillis: 3000,
+      acquireTimeoutMillis: 30000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100,
+      propagateCreateError: false, // <- default is true, set to false
     },
   },
 });
